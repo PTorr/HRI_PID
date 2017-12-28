39f30da9730de3e9db1c5176b5c0557238754fce
“PTorr” <torrpolakow@gmail.com>
2017-12-28 13:38:42 +0200

    pid

--- pid.py
+++ pid.py
@@ -265,7 +265,7 @@ def simulate_kid_data(path, kid, robot, N, prt = True):
     :param N: number of iterations.
     :return: data frame of everything.
     '''
-    krand = 1
+    krand = 1.0
     # PID parameters for all the PID process that we calculate.
     rt_kp, rt_ki, rt_kd       = 1.5, 0, 0.5
     gaze_kp, gaze_ki, gaze_kd = 1.5, 0, 0.5
